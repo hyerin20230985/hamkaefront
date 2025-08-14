395cfbe67aac5e568d3e98cb232dda28f4b8a226
chun9930 <164660514+chun9930@users.noreply.github.com>
2025-08-14 21:47:27 +0900

    test

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import { authAPI } from '../lib/authAPI';
+import { useState } from "react";
 
 const Login = ({ onSwitchToRegister, onLoginSuccess }) => {
     const navigate = useNavigate();
@@ -51,8 +52,6 @@ const Login = ({ onSwitchToRegister, onLoginSuccess }) => {
                     <img src='/hamkae.png' alt='logo' className='w-40'/>
                 </div>
 
-                {error && <div className="error-message">{error}</div>}
-
                 <div className="absolute bottom-0 left-0 right-0">
                     <div
                     className="mx-auto w-full max-w-sm bg-white rounded-t-[28px] shadow-[0_-10px_30px_rgba(0,0,0,0.15)]
@@ -66,14 +65,20 @@ const Login = ({ onSwitchToRegister, onLoginSuccess }) => {
                             placeholder="아이디를 입력해주세요."
                             className="auth-title mt-6 w-full bg-transparent text-gray-800 placeholder-[#73C03F]
                                     border-b-2 border-[#9bd07e] focus:border-[#73C03F] outline-none py-3"
+                            value={formData.username}
+                            onChange={handleChange}
+                            name="username"
                         />
 
                         {/* 비밀번호 */}
                         <input
                             type="password"
+                            name="password"
                             placeholder="비밀번호를 입력해주세요."
                             className="mb-4 mt-4 w-full bg-transparent text-gray-800 placeholder-[#73C03F]
                                     border-b-2 border-[#9bd07e] focus:border-[#73C03F] outline-none py-3"
+                            value={formData.password}
+                            onChange={handleChange}
                         />
 
                         {/* 버튼 */}
@@ -85,7 +90,8 @@ const Login = ({ onSwitchToRegister, onLoginSuccess }) => {
                             active:scale-95
                             focus:outline-none focus-visible:ring-2 focus-visible:ring-[#73C03F]
                             focus-visible:ring-offset-2 focus-visible:ring-offset-white"
-                            type="button"
+                            type="submit"
+                            disabled={loading}
                             >
                             {loading ? '로그인 중...' : '로그인'}
                             </button>
@@ -97,12 +103,13 @@ const Login = ({ onSwitchToRegister, onLoginSuccess }) => {
                             focus:outline-none focus-visible:ring-2 focus-visible:ring-[#A2D07A]
                             focus-visible:ring-offset-2 focus-visible:ring-offset-white"
                             type="button"
-                            onClick={() => navigate("/join")}
+                            onClick={() => navigate("/Register")}
                             >
                             회원가입
                             </button>  
                             </div>
                         </form>
+                        {error && <div className="error-message">{error}</div>}
                     </div>
                 </div>
             </div>
